fc22377e63692fbd4d5ba987700dae04e5a477a8
Gonky10 <nicolasgoncky@gmail.com>
2025-06-26 21:55:17 -0300

    commit 3

--- backend/index.js
+++ backend/index.js
@@ -65,12 +65,13 @@ sequelize.authenticate()
     }
   })
 
-  
+
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
+
 
 
 app.listen(PORT, '0.0.0.0', () => {
