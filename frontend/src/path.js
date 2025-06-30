1a89182636e0c11fc267575558e8b57c182ea846
Gonky10 <nicolasgoncky@gmail.com>
2025-06-30 19:21:33 -0300

    path 2

--- frontend/src/path.js
+++ frontend/src/path.js
@@ -1,6 +1,6 @@
 export const path = {
     src: '/img/',
-    src_url: 'http://localhost:3001/api/secciones/',
+    src_url: '/api/secciones/',
     src_general: 'http://localhost:3001',
     src_back_end: 'http://localhost:3001'
 }
